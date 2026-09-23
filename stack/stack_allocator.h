a697f0cc5b8bdf9807dc41d18471d0d151d51e49
rarr <111408882+rarr294@users.noreply.github.com>
2026-09-23 15:49:10 +0800

    Update stack_allocator.h

--- stack/stack_allocator.h
+++ stack/stack_allocator.h
@@ -1,1 +1,17 @@
+#ifndef STACK_ALLOCATOR_H
+#define STACK_ALLOCATOR_H
 
+#include <stddef.h>
+#include <stdint.h>
+
+struct memory_pool{
+  char *memory;
+  uint64_t size;
+  uint64_t st_off;
+};
+
+char *alloc(struct memory_pool *mem,uint64_t size);
+
+void free_memory(struct memory_pool *mem,char *memory);
+
+#endif
